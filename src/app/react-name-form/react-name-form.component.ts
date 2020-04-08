6effbff53712f190928eb06f00cd477ffb701660
kanwar <kanwar@kanwars-MacBook-Pro.local>
2020-04-08 20:28:00 +0800

    --- Auto-Git Commit ---

--- src/app/react-name-form/react-name-form.component.ts
+++ src/app/react-name-form/react-name-form.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl,FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-react-name-form',
   templateUrl: './react-name-form.component.html',
   styleUrls: ['./react-name-form.component.css']
 })
-export class ReactNameFormComponent implements OnInit {
+export class ReactNameFormComponent  {
+ form= new FormGroup(
+   {
+     userName: new FormControl('',Validators.required,),
+     password: new FormControl('',Validators.required)
+   }
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+ );
 
 }
