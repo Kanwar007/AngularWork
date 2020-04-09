828531b54b33797206135a014136844d69a28907
kanwar <kanwar@kanwars-MacBook-Pro.local>
2020-04-09 15:56:59 +0800

    --- Auto-Git Commit ---

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,7 +8,8 @@ import { ContactFormComponent } from './contact-form/contact-form.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ReactNameFormComponent } from './react-name-form/react-name-form.component';
 import { AddAndRemoveformComponent } from './add-and-removeform/add-and-removeform.component';
-import { NewCourseFormComponent } from './new-course-form/new-course-form.component'
+import { NewCourseFormComponent } from './new-course-form/new-course-form.component';
+import { PostFormComponent } from './post-form/post-form.component'
 
 @NgModule({
   declarations: [
@@ -15,13 +17,15 @@ import { NewCourseFormComponent } from './new-course-form/new-course-form.compon
     ContactFormComponent,
     ReactNameFormComponent,
     AddAndRemoveformComponent,
-    NewCourseFormComponent
+    NewCourseFormComponent,
+    PostFormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
