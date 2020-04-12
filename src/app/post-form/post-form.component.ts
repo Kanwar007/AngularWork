337878d3f350a9eafcb0132779bd919271088a32
kanwar <kanwar@kanwars-MBP.gateway>
2020-04-12 09:46:42 +0800

    --- Auto-Git Commit ---

--- src/app/post-form/post-form.component.ts
+++ src/app/post-form/post-form.component.ts
@@ -1,6 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 
 import { Component, OnInit, Input } from '@angular/core';
+import{GithubUser} from './githubUser'
 
 
 @Component({
@@ -10,26 +11,52 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class PostFormComponent  {
  post : any
-// url='https://jsonplaceholder.typicode.com/posts'
+ url1='https://jsonplaceholder.typicode.com/posts'
  url="https://api.github.com/users"
   constructor(private http: HttpClient ) { 
-    this.http.get(this.url,).subscribe((response: Response)=>
+    this.http.get(this.url,).subscribe((data: GithubUser)=>
       {
       //  console.log(response);
-      console.log(response)
-        this.post = response
+      console.log('TJ user data', data);
+        this.post = data
       }
       
       )
   }
+  ngOnInit(): void {
+   // const url = 'https://api.github.com/users/tj';
+    // Make the GET HTTP request with responseType option
+   // this.http.get(url, {responseType: 'blob'})
+   //   .subscribe((data) => {
+        // Data extraction from the HTTP response is already done
+        // Display the result
+   //     console.log('TJ user data', data);
+  //    });
+  const url = 'https://api.github.com/users/tj';
+  // Make the GET HTTP request with options {observe: 'response'}
+  // The 'observe' option tell HttpClient which data it has to return.
+  this.http.get(url, { observe: 'response' })
+  // Reading the HTTP response
+  // Note the generic
+    .subscribe((response: HttpResponse<GithubUser>) => {
+      console.log('HTTP response', response);
+      console.log('HTTP response : Headers', response.headers);
+      console.log('HTTP response : status', response.status);
+      console.log('HTTP response : url', response.url);
+      // Note that we don't need parse the response, we can access
+      // it directly through 'body' property
+      console.log('HTTP response : body', response.body);
+    });
+    }
 
   creatPost(input : HTMLInputElement){
    let post:any = {title : input.value};
    input.value='';
-    this.http.post(this.url,JSON.stringify(post))
+    this.http.post(this.url1,JSON.stringify(post))
     .subscribe((data :Response)=> {
      // this.post.splice(0,0,post);
       console.log(data);
+      this.post.push(data)
       
       
     });
